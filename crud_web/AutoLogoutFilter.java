c997699f5689d6fe7c38bd2ee53e84e202aa1be0
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2025-01-13 12:57:20 +0500

    Create AutoLogoutFilter.java

---
+++ crud_web/AutoLogoutFilter.java
@@ -0,0 +1,30 @@
+package com.code;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AutoLogoutFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("auto logout filter initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
