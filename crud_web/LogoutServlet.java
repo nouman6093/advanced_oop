a643d32b99121ebfa99de11fbec16be01232e638
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2025-01-13 12:56:56 +0500

    Create LogoutServlet.java

---
+++ crud_web/LogoutServlet.java
@@ -0,0 +1,22 @@
+package com.code;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = {"/LogoutServlet"})
+public class LogoutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        session.invalidate();
+        response.sendRedirect("login.jsp?you%20have%20been%20logged%20out");
+    }
+}
